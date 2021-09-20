b1ec3d4123270371ca6b5a59d2a3cc361f7cb7c3
WangJianYong1124 <19979014715@189.cn>
2021-09-20 16:16:24 +0800

    完成后端添加用户接口开发

---
+++ backend/routes/users.js
@@ -0,0 +1,11 @@
+var express = require('express');
+var router = express.Router();
+
+// 载入中间件
+const { signup, list } = require("../controllers/users")
+
+
+router.post('/signup', signup);
+router.get("/list", list)
+
+module.exports = router;
---
+++ backend/utils/db.js
@@ -0,0 +1,16 @@
+// getting-started.js
+var mongoose = require('mongoose');
+mongoose.connect('mongodb://localhost/zhaopin-admin', {useNewUrlParser: true, useUnifiedTopology: true});
+
+var db = mongoose.connection;
+db.on('error', console.error.bind(console, 'connection error:'));
+
+// 构建users的model
+var usersSchema = mongoose.Schema({
+  username: String,
+  password: String
+});
+
+var Users = mongoose.model('users', usersSchema);
+
+exports.Users = Users
